ff1e979e89b76112cfd9db2affbde602b4604006
Marek Ossowski <marek0ossowski@gmail.com>
2023-02-26 15:17:06 +0100

    Initial commit

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,125 @@
+using NUnit.Framework;
+using multiBaseCalc;
+
+namespace TestProject
+{
+    public class Tests
+    {
+        private Form1 form;
+
+        [SetUp]
+        public void Setup()
+        {
+            form = new multiBaseCalc.Form1();
+        }
+
+
+        [TestCase("", 0.0)]
+        [TestCase("0", 0.0)]
+        [TestCase("0.00", 0.0)]
+        [TestCase("0,00", 0.0)]
+        [TestCase("-0", 0.0)]
+        [TestCase("-0.0", 0.0)]
+        [TestCase("-0,0", 0.0)]
+
+        [TestCase("1", 1.0)]
+        [TestCase("-1", -1.0)]
+        [TestCase("100.0", 100.0)]
+        [TestCase("1234,0", 1234.0)]
+        [TestCase("-444.00", -444.0)]
+        [TestCase("999999", 999999.0)]
+        [TestCase("-999999", -999999.0)]
+        [TestCase("123456789012345", 123456789012345.0)]
+        [TestCase("-123456789012345", -123456789012345.0)]
+
+        [TestCase("1234.5678", 1234.5678)]
+        [TestCase("-1234.5678", -1234.5678)]
+        [TestCase("-1234,5678", -1234.5678)]
+        [TestCase("0.999999", 0.999999)]
+        [TestCase("-0.999999", -0.999999)]
+        [TestCase(".999999", 0.999999)]
+        [TestCase("-.999999", -0.999999)]
+
+        [TestCase("3.1415926535897", 3.1415926535897)]
+        [TestCase("1000000000000000", 1e15)]
+        [TestCase("-5000000000000000", -5e15)]
+        [TestCase("1000000000000000000000000000000", 1e30)]
+        [TestCase("0.001", 1e-3)]
+        [TestCase("0.0000000000000001", 1e-16)]
+        [TestCase("-0.0000000000000000000000000000001", -1e-31)]
+        [TestCase("-0.00000000000000000000000000000012345", -1.2345e-31)]
+        public void TestBase10(string s, double expected)
+        {
+            //Assert.AreEqual(expected, form.StringToDouble(s, 10));
+            Assert.AreEqual(s, form.DoubleToString(expected, 10));
+
+            //FIXME same cases in the other direction?
+            //double actualDouble = form.StringToDouble(s, 10);
+            //string actualString = form.DoubleToString(expected, 10);
+
+            //Assert.IsTrue(actualDouble == expected && actualString == s,
+            //    string.Format("{0} => {1}, {2} => {3}",
+            //    s, actualDouble, expected, actualString));
+        }
+
+        [TestCase("", 0.0)]
+        [TestCase("0", 0.0)]
+        [TestCase("0.00", 0.0)]
+        [TestCase("0,00", 0.0)]
+        [TestCase("-0", 0.0)]
+        [TestCase("-0.0", 0.0)]
+        [TestCase("-0,0", 0.0)]
+
+        [TestCase("1", 1.0)]
+        [TestCase("-1", -1.0)]
+        [TestCase("100.0", 4.0)]
+        [TestCase("11010,0", 26.0)]
+        [TestCase("-110111.00", -55.0)]
+        [TestCase("11110100001000111111", 999999.0)]
+        [TestCase("-11110100001000111111", -999999.0)]
+        [TestCase("011100000100100010000110000011011101111101111001", 123456789012345.0)]
+        [TestCase("-011100000100100010000110000011011101111101111001", -123456789012345.0)]
+
+        [TestCase("1010.0011", 10.1875)]
+        [TestCase("-1010.0011", -10.1875)]
+        [TestCase("-1010,0011", -10.1875)]
+        [TestCase("0.111111", 0.984375)]
+        [TestCase("-0.111111", -0.984375)]
+        [TestCase(".111111", 0.984375)]
+        [TestCase("-.111111", -0.984375)]
+
+        [TestCase("11.0010010000111111", 3.1415863037109375)]
+        [TestCase("1000000000000000", 32768.0)]
+        [TestCase("-101000000000000000", -163840.0)]
+        [TestCase("1000000000000000000000000000000", 1073741824.0)]
+        [TestCase("1000000000000000000000000000000000000000000000000000000000000", 1152921504606846976.0)]
+        [TestCase("0.001", 0.125)]
+        [TestCase("0.0000000000000001", 0.0000152587890625)]
+        [TestCase("-0.0000000000000000000000000000001", -0.0000000004656612873077392578125)]
+        [TestCase("-0.000000000000000000000000000000101", -5.82076609134674072265625e-10)]
+        public void TestBase2(string s, double expected)
+        {
+            Assert.AreEqual(expected, form.StringToDouble(s, 2));
+        }
+
+        [TestCase("", 0.0)]
+        [TestCase("0", 0.0)]
+        [TestCase("0.00", 0.0)]
+
+        [TestCase("1", 1.0)]
+        [TestCase("-1", -1.0)]
+        [TestCase("100.0", 256.0)]
+        [TestCase("c1,0", 193.0)]
+        [TestCase("-1f.00", -31.0)]
+
+        [TestCase("c.4", 12.25)]
+        [TestCase("-f.fa", -15.9765625)]
+
+        [TestCase("3.243F6A89", 3.14159265346825122833251953125)]
+        [TestCase("0.001", 0.000244140625)]
+        public void TestBase16(string s, double expected)
+        {
+            Assert.AreEqual(expected, form.StringToDouble(s, 16));
+        }
+    }
+}
---
+++ multiBaseCalc/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace multiBaseCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Consolas", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(-1, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(531, 63);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1234567890abcdef";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 152);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
