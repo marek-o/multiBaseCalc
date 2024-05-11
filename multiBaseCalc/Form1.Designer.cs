b161e62f567be907eb051869b915ef835bf1a5a8
Marek Ossowski <marek0ossowski@gmail.com>
2024-05-11 14:55:15 +0200

    Enabled ForceDesignerDpiUnaware

--- multiBaseCalc/Form1.Designer.cs
+++ multiBaseCalc/Form1.Designer.cs
@@ -29,44 +29,43 @@ namespace multiBaseCalc
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.labelBase = new System.Windows.Forms.Label();
-            this.SuspendLayout();
+            label1 = new System.Windows.Forms.Label();
+            labelBase = new System.Windows.Forms.Label();
+            SuspendLayout();
             // 
             // label1
             // 
-            this.label1.Font = new System.Drawing.Font("Consolas", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(-1, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(531, 63);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "1234567890abcdef";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            label1.Font = new System.Drawing.Font("Consolas", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label1.Location = new System.Drawing.Point(-1, 9);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(531, 63);
+            label1.TabIndex = 0;
+            label1.Text = "1234567890abcdef";
+            label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // labelBase
             // 
-            this.labelBase.AutoSize = true;
-            this.labelBase.Location = new System.Drawing.Point(480, 90);
-            this.labelBase.Name = "labelBase";
-            this.labelBase.Size = new System.Drawing.Size(38, 15);
-            this.labelBase.TabIndex = 0;
-            this.labelBase.Text = "label2";
-            this.labelBase.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            labelBase.AutoSize = true;
+            labelBase.Location = new System.Drawing.Point(480, 90);
+            labelBase.Name = "labelBase";
+            labelBase.Size = new System.Drawing.Size(38, 15);
+            labelBase.TabIndex = 0;
+            labelBase.Text = "label2";
+            labelBase.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(530, 152);
-            this.Controls.Add(this.labelBase);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
-            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(530, 152);
+            Controls.Add(labelBase);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            KeyDown += Form1_KeyDown;
+            KeyPress += Form1_KeyPress;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
